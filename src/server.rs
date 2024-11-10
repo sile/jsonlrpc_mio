8fa99e37107ebf998f10aa52e8dbca5b7dd30373
Takeru Ohta <phjgt308@gmail.com>
2024-11-10 21:32:52 +0900

    Add handle_accepted()

--- src/server.rs
+++ src/server.rs
@@ -5,7 +5,12 @@ use std::{
     net::SocketAddr,
 };
 
-use mio::{event::Event, net::TcpListener, Interest, Poll, Token};
+use jsonlrpc::JsonlStream;
+use mio::{
+    event::Event,
+    net::{TcpListener, TcpStream},
+    Interest, Poll, Token,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
@@ -74,11 +79,49 @@ where
     }
 
     fn handle_listener_event(&mut self, poller: &mut Poll) -> std::io::Result<()> {
-        todo!()
+        loop {
+            match self.listener.accept() {
+                Err(e) if e.kind() == ErrorKind::WouldBlock => break,
+                Err(e) => return Err(e),
+                Ok((stream, _addr)) => {
+                    let Some(connection) = self.handle_accepted(poller, stream) else {
+                        continue;
+                    };
+                    self.connections.insert(connection.token, connection);
+                }
+            }
+        }
+        Ok(())
     }
 
-    pub fn try_recv(&mut self) -> Option<(From, REQ)> {
-        self.requests.pop_front()
+    fn handle_accepted(&mut self, poller: &mut Poll, mut stream: TcpStream) -> Option<Connection> {
+        stream.set_nodelay(true).ok()?;
+        let token = self.next_token()?;
+        poller
+            .registry()
+            .register(&mut stream, token, Interest::READABLE)
+            .ok()?;
+        Some(Connection {
+            token,
+            stream: JsonlStream::new(stream),
+        })
+    }
+
+    fn next_token(&mut self) -> Option<Token> {
+        if self.token_end.0 - self.token_start.0 == self.connections.len() + 1 {
+            return None;
+        }
+
+        loop {
+            let token = self.next_token;
+            self.next_token.0 += 1;
+            if self.next_token == self.token_end {
+                self.next_token.0 = self.token_start.0 + 1;
+            }
+            if !self.connections.contains_key(&token) {
+                return Some(token);
+            }
+        }
     }
 
     pub fn reply<T: Serialize>(&mut self, from: From, response: &T) -> serde_json::Result<()> {
@@ -93,7 +136,10 @@ pub struct From {
 
 // TODO(?): RpcServerConnection
 #[derive(Debug)]
-struct Connection {}
+struct Connection {
+    token: Token,
+    stream: JsonlStream<TcpStream>,
+}
 
 impl Connection {
     fn handle_event(&mut self, poller: &mut Poll, event: &Event) -> std::io::Result<()> {
